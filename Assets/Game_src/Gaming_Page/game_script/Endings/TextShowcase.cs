129a7ce766d417d20a4bc2dfc547f69853a14586
Menma2021 <sagt22052006@gmail.com>
2024-10-28 16:22:38 +0000

    Added ending scenes
    1st - good credit
    2nd - bad credit
    3rd - didn't make a decision for a while
    4th - rejected everyone from the day
    
    Also added typing sound

---
+++ Assets/Game_src/Gaming_Page/game_script/Endings/TextShowcase.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TextShowcase : MonoBehaviour
+{
+    [TextArea]
+    public string[] endingTexts;
+    public float typingSpeed = 0.05f;
+    public TextMeshProUGUI uiTextMeshPro;
+
+    public AudioClip typingSound;
+    private bool isFinishedTyping = false;
+    private bool isTypingSoundPlaying = false;
+
+    private int currentPage = 0;
+    float elapsedTime = 0;
+
+    private void Awake()
+    {
+        StartCoroutine(DisplayText());
+    }
+
+    private IEnumerator DisplayText()
+    {
+        if (uiTextMeshPro != null)
+        {
+            uiTextMeshPro.text = "";
+        }
+
+        while (currentPage < endingTexts.Length)
+        {
+
+            foreach (char letter in endingTexts[currentPage])
+            {
+                if (uiTextMeshPro != null)
+                {
+                    uiTextMeshPro.text += letter;
+                }
+
+                if (!isTypingSoundPlaying)
+                {
+                    isTypingSoundPlaying = true;
+                    AudioManager.instance.PlayEnvironmentSound(typingSound);
+                }
+
+                yield return new WaitForSeconds(typingSpeed);
+            }
+            if (currentPage + 1 < endingTexts.Length)
+            {
+                AudioManager.instance.environmentSource.Stop();
+                isTypingSoundPlaying = false;
+                yield return new WaitForSeconds(2);
+                uiTextMeshPro.text = "";
+            }
+            currentPage++;
+        }
+        isFinishedTyping = true;
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= 6.24)
+        {
+            elapsedTime = 0;
+            isTypingSoundPlaying = false;
+        }
+
+        if (isFinishedTyping && Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadMainMenu();
+        }
+
+        if (isFinishedTyping && isTypingSoundPlaying)
+        {
+            AudioManager.instance.environmentSource.Stop();
+            isTypingSoundPlaying = false;
+        }
+    }
+
+    private void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
