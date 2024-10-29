e9596b81b49b373c412953b10cee1d3850579dbc
Menma2021 <sagt22052006@gmail.com>
2024-10-29 15:01:55 +0000

    More changes

--- Assets/Game_src/Gaming_Page/game_script/multi_windows/NetworkConnection.cs
+++ Assets/Game_src/Gaming_Page/game_script/multi_windows/NetworkConnection.cs
@@ -87,37 +87,6 @@ public class NetworkConnection : NetworkBehaviour
         start_become_host();
         OnNetDone?.Invoke();
     }
-    void OnGUI()
-    {
-        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-        {
-            StartButtons();
-        }
-        else
-        {
-            StatusLabels();
-        }
-
-        GUILayout.EndArea();
-    }
-
-    static void StartButtons()
-    {
-        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
-    }
-
-    static void StatusLabels()
-    {
-        var mode = NetworkManager.Singleton.IsHost ?
-           "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
-
-        GUILayout.Label("Transport: " +
-           NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-        GUILayout.Label("Mode: " + mode);
-    }
 
 
     // private void Start()
